3e945f28c616b55c8228cb3acb5ff29ce88fbaf1
Nidhi Chandra <117846833+nidhichandra17@users.noreply.github.com>
2023-07-03 23:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function getSize(){
+	 const w = document.documentElement.clientWidth;
+     const h = document.documentElement.clientHeight;
+
+	const elem = document.getElementById('sizeInfo');
+	elem.innerHTML="<h1> Width: "+w+" and Height: "+h+"</h1";
+}
